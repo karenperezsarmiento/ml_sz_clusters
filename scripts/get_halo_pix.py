40c0a485234877b25ebc280499f3183db8d36373
Karen Perez Sarmiento kaper@sas.upenn.edu <kaper@gpc2b.sas.upenn.edu>
2023-09-21 12:14:35 -0400

    first commit

---
+++ scripts/get_halo_pix.py
@@ -0,0 +1,54 @@
+import healpy as hp
+from cosmology import *
+import numpy as np
+import pandas as pd
+import astropy.table as table
+
+rho = 2.775e11*omegam*h**2
+f=open('../websky/halos.pksc')
+N=np.fromfile(f,count=3,dtype=np.int32)[0]
+
+catalog = np.fromfile(f,count=N*10,dtype=np.float32)
+catalog = np.reshape(catalog,(N,10))
+
+x  = catalog[:,0];  y = catalog[:,1];  z = catalog[:,2] # Mpc (comoving)
+vx = catalog[:,3]; vy = catalog[:,4]; vz = catalog[:,5] # km/sec
+R  = catalog[:,6] # Mpc
+
+M200m    = 4*np.pi/3.*rho*R**3
+chi      = np.sqrt(x**2+y**2+z**2) #Mpc
+vrad     = (x*vx + y*vy + z*vz) / chi #km/s
+redshift = zofchi(chi)
+
+theta,phi = hp.vec2ang(np.column_stack((x,y,z))) #radians
+nside_1 = 2048
+nside_2 = 4092
+nside_3 = 8192
+
+#map_1 =np.zeros((hp.nside2npix(nside_1)))
+#map_2 =np.zeros((hp.nside2npix(nside_2)))
+
+pix_1 = hp.vec2pix(nside_1, x, y, z)
+pix_2 = hp.vec2pix(nside_2, x, y, z)
+pix_3 = hp.vec2pix(nside_3, x, y, z)
+
+decDeg=-1*(np.degrees(theta)-90)
+raDeg = np.degrees(phi)
+
+names=[]
+for i in range(len(raDeg)):
+    n = 'MOCK-CL_'+ str(i)
+    names.append(n)
+
+outFileName="../websky/halos.fits"
+tab = table.Table()
+tab.add_column(table.Column(names, 'name'))
+tab.add_column(table.Column(raDeg, 'RADeg'))
+tab.add_column(table.Column(decDeg, 'decDeg'))
+tab.add_column(table.Column(M200m, 'M200m'))
+tab.add_column(table.Column(redshift, "z"))
+tab=tab[np.where(tab['M200m'] >= 0.5e14)]
+tab=tab[np.where(tab['M200m'] <= 1e15)]
+##add redshift filter
+tab.write(outFileName,overwrite=True)
+print("Websky created")
---
+++ scripts/get_training_data.py
@@ -0,0 +1,101 @@
+import pixell
+from pixell import reproject,enmap,utils
+import numpy as np
+import healpy as hp
+from astropy.io import fits
+import astropy.units as u
+import matplotlib.pyplot as plt
+import json
+import jsonlines
+import argparse as ap
+
+print("Initialized")
+parser = ap.ArgumentParser(description="Create cutouts around websky halos to use as input in CNN")
+parser.add_argument("-of","--output_file",type=str,help="Name of output file")
+parser.add_argument("-s","--size",type=str,help="Size of output dataset (mini,small or full")
+parser.add_argument("-nc","--num_channels",type=int,help="Number of channels in output cutout (1 or 3)")
+args = parser.parse_args()
+output_fn = args.output_file
+output_sz = args.size
+nchannels = args.num_channels
+print("output filename "+output_fn)
+print("size of dataset "+output_sz)
+print("num channels "+str(nchannels))
+
+res = np.deg2rad(0.5 / 60.)
+
+tot_145_map = "../websky/tot_145.fits"
+tot_93_map = "../websky/tot_93.fits"
+tot_217_map = "../websky/tot_217.fits"
+
+tsz_map = "../websky/tsz_8192.fits"
+halo_coords = "../websky/halos.fits"
+width = 16*utils.arcmin
+
+z_max = 4.6
+mass_min = 0.5e14
+mass_max = 1e15
+
+def healpy_2_pixell(filename,res):
+    shape,wcs = enmap.fullsky_geometry(res=res)
+    h_map = hp.read_map(filename).astype(np.float64)
+    p_map = reproject.healpix2map(h_map,shape=shape,wcs=wcs)
+    min_val = np.min(p_map)
+    max_val = np.max(p_map)
+    return p_map, min_val, max_val
+
+def get_cutouts(ra,dec,p_map,width=10.0*utils.arcmin,res=res):
+    ra,dec = ra*u.deg,dec*u.deg
+    cutouts = reproject.thumbnails(p_map,(dec.to(u.radian).value,ra.to(u.radian).value),r=width,res=res)
+    return cutouts
+
+def load_coords(coord_file):
+    hdul = fits.open(coord_file)
+    data = hdul[1].data 
+    return data["name"],data["RADeg"],data["decDeg"],data["M200m"],data["z"]
+
+idx,ra,dec,m200,z = load_coords(halo_coords)
+
+maps_dict = {}
+files = ["tot_93","tot_145","tot_217","tsz_8192"]
+for i in files:
+    f = "../websky/"+ i + ".fits"
+    p_map, min_val, max_val = healpy_2_pixell(f,res)
+    maps_dict[i] = {
+        "p_map" : p_map,
+        "min" : min_val,
+        "max" : max_val,    
+    }
+    
+ofn = "../data/"+output_fn
+if output_sz=="mini":
+    osize = 1000
+elif output_sz=="small":
+    osize = 20000
+else:
+    osize = len(ra)
+with jsonlines.open(ofn,mode="w") as write_file:
+    for i in range(osize):
+        if nchannels == 3:
+            z_norm = z[i]/z_max
+            m200_norm = (m200[i]-mass_min)/(mass_max-mass_min)
+            z_cutout = np.ones((64,64))*z_norm
+            m_cutout = np.ones((64,64))*m200_norm 
+        cutout_dict = {}
+        cutout_dict["name"]=str(idx[i])
+        for k in maps_dict.keys():
+            cutout = get_cutouts(ra[i],dec[i],maps_dict[k]["p_map"],width=width,res=res)
+            cutout = cutout[:64,:64]
+            cutout_norm = (cutout - maps_dict[k]["min"])/(maps_dict[k]["max"]-maps_dict[k]["min"])
+            if nchannels == 3:
+                if k != "tsz_8192":
+                    cutout_out = np.dstack((cutout_norm,m_cutout,z_cutout)) 
+                else:
+                    cutout_out = cutout_norm
+            else:
+                cutout_out = cutout_norm
+            cutout_dict[k] = cutout_out.tolist()
+        write_file.write(cutout_dict)
+
+write_file.close()
+print("Created input dataset (normalized)")
