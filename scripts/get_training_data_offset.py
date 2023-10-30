aff655a247ccae06f55e524a036da61622c3b3cb
Abigail Harris <avharris@sas.upenn.edu>
2023-10-30 13:54:57 -0400

    Added offset to get_training_data.py

--- scripts/get_training_data_offset.py
+++ scripts/get_training_data_offset.py
@@ -14,22 +14,25 @@ parser = ap.ArgumentParser(description="Create cutouts around websky halos to us
 parser.add_argument("-of","--output_file",type=str,help="Name of output file")
 parser.add_argument("-s","--size",type=str,help="Size of output dataset (mini,small or full")
 parser.add_argument("-nc","--num_channels",type=int,help="Number of channels in output cutout (1 or 3 or 5)")
+parser.add_argument("-offs","--offset", type=float, help="Maximum offset from center in radians")
 args = parser.parse_args()
 output_fn = args.output_file
 output_sz = args.size
 nchannels = args.num_channels
+max_offset = args.offset
 print("output filename "+output_fn)
 print("size of dataset "+output_sz)
 print("num channels "+str(nchannels))
+print("max offset "+str(max_offset))
 
 res = np.deg2rad(0.5 / 60.)
 
-tot_145_map = "../websky/tot_145.fits"
-tot_93_map = "../websky/tot_93.fits"
-tot_217_map = "../websky/tot_217.fits"
+tot_145_map = "/home3/kaper/ml_sz_clusters/websky/tot_145.fits"
+tot_93_map = "/home3/kaper/ml_sz_clusters/websky/tot_93.fits"
+tot_217_map = "/home3/kaper/ml_sz_clusters/websky/tot_217.fits"
 
-tsz_map = "../websky/tsz_8192.fits"
-halo_coords = "../websky/halos.fits"
+tsz_map = "/home3/kaper/ml_sz_clusters/websky/tsz_8192.fits"
+halo_coords = "/home3/kaper/ml_sz_clusters/websky/halos.fits"
 width = 16*utils.arcmin
 
 z_max = 4.6
@@ -44,14 +47,18 @@ def healpy_2_pixell(filename,res):
     max_val = np.max(p_map)
     return p_map, min_val, max_val
 
-def get_cutouts(ra,dec,p_map,width=10.0*utils.arcmin,res=res):
+def get_cutouts(ra,dec,p_map,width=10.0*utils.arcmin,res=res, max_offset=5.0*utils.arcmin):
+    # max_offset is in radians and gives the maximum amount that the thumbnail will be off center. For 
+    # a max_offset of half of the width (converted to radians), the furthest
+    # that the cutout may be off-center is half the width in the ascension direction and half the width
+    # in the declination direction
     ra,dec = ra*u.deg,dec*u.deg
-    cutouts = reproject.thumbnails(p_map,(dec.to(u.radian).value,ra.to(u.radian).value),r=width,res=res)
+    cutouts = reproject.thumbnails(p_map,(dec.to(u.radian).value+max_offset*uniform(-1,1),ra.to(u.radian).value+max_offset*uniform(-1,1)),r=width,res=res)
     return cutouts
 
 def load_coords(coord_file):
     hdul = fits.open(coord_file)
-    data = hdul[1].data 
+    data = hdul[1].data
     return data["name"],data["RADeg"],data["decDeg"],data["M200m"],data["z"]
 
 idx,ra,dec,m200,z = load_coords(halo_coords)
@@ -59,15 +66,15 @@ idx,ra,dec,m200,z = load_coords(halo_coords)
 maps_dict = {}
 files = ["tot_93","tot_145","tot_217","tsz_8192"]
 for i in files:
-    f = "../websky/"+ i + ".fits"
+    f = "/home3/kaper/ml_sz_clusters/websky/"+ i + ".fits"
     p_map, min_val, max_val = healpy_2_pixell(f,res)
     maps_dict[i] = {
         "p_map" : p_map,
         "min" : min_val,
-        "max" : max_val,    
+        "max" : max_val,
     }
-    
-ofn = "../data/"+output_fn
+
+ofn = output_fn
 if output_sz=="mini":
     osize = 1000
 elif output_sz=="small":
@@ -80,7 +87,7 @@ with jsonlines.open(ofn,mode="w") as write_file:
             z_norm = z[i]/z_max
             m200_norm = (m200[i]-mass_min)/(mass_max-mass_min)
             z_cutout = np.ones((64,64))*z_norm
-            m_cutout = np.ones((64,64))*m200_norm 
+            m_cutout = np.ones((64,64))*m200_norm
             cutout_out = np.dstack((m_cutout,z_cutout))
         cutout_dict = {}
         cutout_dict["name"]=str(idx[i])
@@ -89,14 +96,14 @@ with jsonlines.open(ofn,mode="w") as write_file:
         elif nchannels == 5:
             list_freqs = ["tot_93","tot_145","tot_217"]
         for k in list_freqs:
-            cutout = get_cutouts(ra[i],dec[i],maps_dict[k]["p_map"],width=width,res=res)
+            cutout = get_cutouts(ra[i],dec[i],maps_dict[k]["p_map"],width=width,res=res,max_offset=max_offset)
             cutout = cutout[:64,:64]
             cutout_norm = (cutout - maps_dict[k]["min"])/(maps_dict[k]["max"]-maps_dict[k]["min"])
             if (nchannels == 5) or (nchannels == 3):
                 cutout_out = np.dstack((cutout_out,cutout_norm))
             elif nchannels == 1:
                 cutout_out = cutout_norm
-        
+
         cutout_dict["tot"] = cutout_out.tolist()
         cutout_label = get_cutouts(ra[i],dec[i],maps_dict["tsz_8192"]["p_map"],width=width,res=res)
         cutout_label = cutout_label[:64,:64]
@@ -106,3 +113,4 @@ with jsonlines.open(ofn,mode="w") as write_file:
 
 write_file.close()
 print("Created input dataset (normalized)")
+
