8dc326599ad6f2a4d23ed8f11484a1902435ab88
Abigail Harris <avharris@sas.upenn.edu>
2023-11-28 19:28:35 -0500

    Added plotting norm of residual vs offset to subtract.py

--- scripts/get_training_data_offset.py
+++ scripts/get_training_data_offset.py
@@ -14,16 +14,19 @@ parser = ap.ArgumentParser(description="Create cutouts around websky halos to us
 parser.add_argument("-of","--output_file",type=str,help="Name of output file")
 parser.add_argument("-s","--size",type=str,help="Size of output dataset (mini,small or full")
 parser.add_argument("-nc","--num_channels",type=int,help="Number of channels in output cutout (1 or 3 or 5)")
-parser.add_argument("-offs","--offset", type=float, help="Maximum offset from center in radians")
+parser.add_argument("-ns","--noise",type=bool,help="Include noise")
+parser.add_argument("-bm","--beam",type=bool,help="Include beam")
 args = parser.parse_args()
 output_fn = args.output_file
 output_sz = args.size
 nchannels = args.num_channels
-max_offset = args.offset
+noise = False
+beam = False
 print("output filename "+output_fn)
 print("size of dataset "+output_sz)
 print("num channels "+str(nchannels))
-print("max offset "+str(max_offset))
+print("include noise "+str(noise))
+print("include beam "+str(beam))
 
 res = np.deg2rad(0.5 / 60.)
 
@@ -39,6 +42,25 @@ z_max = 4.6
 mass_min = 0.5e14
 mass_max = 1e15
 
+beam_dict = {"tot_93":2.2,"tot_145":1.4,"tot_217":1.0}
+noise_val = 10 #uK*arcmin
+
+def gauss_beam(ell,fwhm):
+    tht_fwhm= np.deg2rad(fwhm/60.)
+    return np.exp(-(tht_fwhm**2.)*(ell**2.) / (16.*np.log(2.)))
+
+def conv_beam_noise(p_map,freq,add_noise):
+    shape,wcs = enmap.fullsky_geometry(res=res)
+    imap = enmap.zeros(shape, wcs=wcs)
+    alm = pixell.curvedsky.map2alm(p_map,lmax=10000)
+    alm_conv = pixell.curvedsky.almxfl(alm,lambda ell:gauss_beam(ell,beam_dict[freq]))
+    if add_noise:
+        alm_out = pixell.curvedsky.almxfl(alm,lambda ell: ell*0 + noise_val)
+    else:
+        alm_out = alm_conv
+    p_map_conv = pixell.curvedsky.alm2map(alm_out,imap)
+    return p_map_conv
+
 def healpy_2_pixell(filename,res):
     shape,wcs = enmap.fullsky_geometry(res=res)
     h_map = hp.read_map(filename).astype(np.float64)
@@ -47,34 +69,34 @@ def healpy_2_pixell(filename,res):
     max_val = np.max(p_map)
     return p_map, min_val, max_val
 
-def get_cutouts(ra,dec,p_map,width=10.0*utils.arcmin,res=res, max_offset=5.0*utils.arcmin):
-    # max_offset is in radians and gives the maximum amount that the thumbnail will be off center. For 
-    # a max_offset of half of the width (converted to radians), the furthest
-    # that the cutout may be off-center is half the width in the ascension direction and half the width
-    # in the declination direction
+def get_cutouts(ra,dec,p_map,ra_offset,dec_offset,width=10.0*utils.arcmin,res=res):
     ra,dec = ra*u.deg,dec*u.deg
-    cutouts = reproject.thumbnails(p_map,(dec.to(u.radian).value+max_offset*uniform(-1,1),ra.to(u.radian).value+max_offset*uniform(-1,1)),r=width,res=res)
+    ra_off = ra.to(u.radian)+ra_offset
+    dec_off = dec.to(u.radian)+dec_offset
+    cutouts = reproject.thumbnails(p_map,(dec_off.value,ra_off.value),r=width,res=res)
     return cutouts
 
 def load_coords(coord_file):
     hdul = fits.open(coord_file)
-    data = hdul[1].data
+    data = hdul[1].data 
     return data["name"],data["RADeg"],data["decDeg"],data["M200m"],data["z"]
-
 idx,ra,dec,m200,z = load_coords(halo_coords)
-
 maps_dict = {}
 files = ["tot_93","tot_145","tot_217","tsz_8192"]
 for i in files:
     f = "/home3/kaper/ml_sz_clusters/websky/"+ i + ".fits"
     p_map, min_val, max_val = healpy_2_pixell(f,res)
+    if beam & (i!="tsz_8192"):
+        p_map_out = conv_beam_noise(p_map,freq = i,add_noise = noise)
+    else:
+        p_map_out = p_map
     maps_dict[i] = {
-        "p_map" : p_map,
+        "p_map" : p_map_out,
         "min" : min_val,
-        "max" : max_val,
+        "max" : max_val,    
     }
-
-ofn = output_fn
+    
+ofn = "../data/"+output_fn
 if output_sz=="mini":
     osize = 1000
 elif output_sz=="small":
@@ -87,7 +109,7 @@ with jsonlines.open(ofn,mode="w") as write_file:
             z_norm = z[i]/z_max
             m200_norm = (m200[i]-mass_min)/(mass_max-mass_min)
             z_cutout = np.ones((64,64))*z_norm
-            m_cutout = np.ones((64,64))*m200_norm
+            m_cutout = np.ones((64,64))*m200_norm 
             cutout_out = np.dstack((m_cutout,z_cutout))
         cutout_dict = {}
         cutout_dict["name"]=str(idx[i])
@@ -95,22 +117,23 @@ with jsonlines.open(ofn,mode="w") as write_file:
             list_freqs = ["tot_93"]
         elif nchannels == 5:
             list_freqs = ["tot_93","tot_145","tot_217"]
+        max_offset = (16*u.arcmin/2).to(u.radian)
+        ra_offset = max_offset*np.random.uniform(-1,1,1)[0]
+        dec_offset = max_offset*np.random.uniform(-1,1,1)[0]
+        cutout_dict["offset"] = (dec_offset.to(u.radian).value, ra_offset.to(u.radian).value)
         for k in list_freqs:
-            cutout = get_cutouts(ra[i],dec[i],maps_dict[k]["p_map"],width=width,res=res,max_offset=max_offset)
+            cutout = get_cutouts(ra[i],dec[i],maps_dict[k]["p_map"],ra_offset = ra_offset,dec_offset = dec_offset,width=width,res=res)
             cutout = cutout[:64,:64]
             cutout_norm = (cutout - maps_dict[k]["min"])/(maps_dict[k]["max"]-maps_dict[k]["min"])
             if (nchannels == 5) or (nchannels == 3):
                 cutout_out = np.dstack((cutout_out,cutout_norm))
             elif nchannels == 1:
-                cutout_out = cutout_norm
-
+                cutout_out = cutout_norm        
         cutout_dict["tot"] = cutout_out.tolist()
-        cutout_label = get_cutouts(ra[i],dec[i],maps_dict["tsz_8192"]["p_map"],width=width,res=res)
+        cutout_label = get_cutouts(ra[i],dec[i],maps_dict["tsz_8192"]["p_map"],ra_offset = ra_offset,dec_offset = dec_offset, width=width,res=res)
         cutout_label = cutout_label[:64,:64]
         cutout_label_norm = (cutout_label - maps_dict["tsz_8192"]["min"])/(maps_dict["tsz_8192"]["max"]-maps_dict["tsz_8192"]["min"])
         cutout_dict["tsz_8192"] = cutout_label_norm.tolist()
         write_file.write(cutout_dict)
-
 write_file.close()
 print("Created input dataset (normalized)")
-
--- scripts/subtract.py
+++ scripts/subtract.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import re
 from pixell import reproject,enmap,utils
 import healpy as hp
+from scipy.stats import pearsonr
 
 res = np.deg2rad(0.5 / 60.)
 
@@ -24,7 +25,7 @@ flabels = {2:"93",3:"145",4:"217"}
 f_arr = [2,3,4,5]
 n = {2:-4.2840e6,3:-2.7685e6,4:-2.1188e4}
 for i in range(len(files)):
-    f = "../websky/"+ files[i] + ".fits"
+    f = "/data6/kaper/ml_sz_clusters/websky/"+ files[i] + ".fits"
     min_val, max_val = map_min_max(f,res)
     maps_dict[f_arr[i]] = {
         "min" : min_val,
@@ -33,17 +34,17 @@ for i in range(len(files)):
 
 
 model_fn = "sep_conv2d_linear_small_5ch.keras"
-act_func = "linear"
+act_func = "linear_offset"
 img_dir = "../test_imgs_temp_"+act_func+"/"
-r_model = keras.models.load_model("../models/trained_models/"+model_fn)
-input_fn = "small_5ch.jsonl"
+r_model = keras.models.load_model("/data6/kaper/ml_sz_clusters/models/"+model_fn)
+input_fn = "offset_small_5ch.jsonl"
 data = load_dataset("json",data_files="../data/"+input_fn,split="train")
 
 train_testvalid = data.train_test_split(test_size=0.05)
 
 df_test = train_testvalid["test"].to_tf_dataset(
         columns=["tot"],
-        label_cols=["tsz_8192"],
+        label_cols=["tsz_8192", "offset"],
         batch_size=64,
         prefetch=False,
         shuffle=False)
@@ -57,8 +58,12 @@ res_flat = []
 res_tot = np.zeros((64,64))
 sub_tot = {2:np.zeros((64,64)),3:np.zeros((64,64)),4:np.zeros((64,64))}
 sub_flat = {2:[],3:[],4:[]}
+res_norms = []    # the norm of all pixels in the residual
+off_norms = []    # the norm of the offset in radians. off_norm[i] corresponds to res_norm[i]
 for inputs,labels in df_test.map(lambda x,y: (x,y)):
-    larr = labels.numpy()
+    larr = np.array(labels["tsz_8192"])
+    offset = np.array(labels["offset"])
+    off_norms.extend(np.linalg.norm(offset, axis = 1))
     iarr = inputs.numpy()
     for l in range(larr.shape[0]):
         resfile = img_dir+ifile + str(i)+"_res_sep_conv2d_"+act_func+".png"
@@ -67,6 +72,7 @@ for inputs,labels in df_test.map(lambda x,y: (x,y)):
         res = lab - pred
         res_tot = res_tot + res
         res_flat = np.append(res_flat,res.flatten())
+        res_norms.append(np.linalg.norm(res))
         fig = plt.figure()
         plt.imshow(res)
         plt.savefig(resfile)
@@ -102,7 +108,6 @@ for inputs,labels in df_test.map(lambda x,y: (x,y)):
             plt.close(fig)
         i+=1
 
-
 fig = plt.figure()
 plt.hist(res_flat/i)
 plt.savefig("../plots/"+ifile+"_"+act_func+"_stack_res_hist.png")
@@ -111,6 +116,18 @@ plt.close(fig)
 fig = plt.figure()
 plt.imshow(res_tot/i)
 plt.savefig("../plots/"+ifile+"_"+act_func+"_stack_res.png")
+plt.close(fig)
+
+# Plot the goodness of the fit against the offset
+fig = plt.figure()
+plt.scatter(off_norms, res_norms)
+a, b = np.polyfit(off_norms, res_norms, 1)
+plt.plot(off_norms, a*np.array(off_norms)+b, "-r")
+corr_coeff = str(getattr(pearsonr(off_norms, res_norms), "statistic"))
+plt.title("Pearson correlation coefficient: " + corr_coeff)
+plt.xlabel("Offset norm [radians]")
+plt.ylabel("Norm of all pixels in residual")
+plt.savefig("../plots/"+ifile+"_"+act_func+"_res_vs_offset.png")
 plt.close(fig)
 
 for f in [2,3,4]:
